4423f967bc77bd0b7170aafb8086bb011c06507e
iammushi2089 <samsonjohnrey@gmail.com>
2026-04-19 22:57:25 +0800

    prep for render

--- backend/server.js
+++ backend/server.js
@@ -31,12 +31,11 @@ app.use('/api/posts', postRoutes);
 app.use('/api/comments', commentRoutes);
 app.use('/api/admin', adminRoutes);
 app.use('/api/messages', messageRoutes);
+
 // Basic test route
 app.get('/', (req, res) => {
   res.send('TheFolio API is running...');
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
